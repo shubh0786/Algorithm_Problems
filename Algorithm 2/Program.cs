6124247d5ed92a608c8137762f6e8a79ed346576
shubh0786 <shubham.singh267@gmail.com>
2021-01-26 20:59:57 +1300

    Algorithm 2

---
+++ Algorithm 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a C# Sharp program to get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.
+
+
+namespace Algorithm_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(test(53));
+            Console.WriteLine(test(53));
+            Console.WriteLine(test(51));
+            Console.ReadLine();
+
+        }
+
+
+
+        public static int test(int n)
+        {
+
+            var x = 51;
+
+            if (n > x)
+            {
+
+              
+
+
+                return (n - x) * 3;
+
+
+
+            }
+            else
+            {
+                return x - n;
+            }
+
+
+
+
+        }
+    }
+}
