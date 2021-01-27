15286ec29418ca932060fc5c8c6bd2253d08aa83
shubh0786 <shubham.singh267@gmail.com>
2021-01-27 23:27:18 +1300

    Algo3

---
+++ Algorithm3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//3. Write a C# Sharp program to check two given integers, and return true if one of them is 30 or if their sum is 30. 
+
+namespace Algorithm3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(30,0));
+            Console.WriteLine(test(25, 5));
+            Console.WriteLine(test(20, 30));
+            Console.WriteLine(test(30, 0));
+            Console.WriteLine(test(20, 0));
+
+
+            Console.ReadLine();
+        }
+
+        public static bool test(int n, int y)
+        {
+            return n == 30 || y == 30 || (n + y == 30);
+        }
+
+
+    }
+}
